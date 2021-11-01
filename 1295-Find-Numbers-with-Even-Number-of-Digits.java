61267aa634138b80cd619960ab7887275ce9b745
Murlidhar Pathak <47478882+murlidharpathakprogrammer@users.noreply.github.com>
2021-11-01 22:38:07 +0530

    Create 1295-Find-Numbers-with-Even-Number-of-Digits.java

---
+++ 1295-Find-Numbers-with-Even-Number-of-Digits.java
@@ -0,0 +1,5 @@
+/*
+Problem: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+         Given an array nums of integers, return how many of them contain an even number of digits.
+My Submission: https://leetcode.com/submissions/detail/580529526/
+*/
