e91ce0a61b1e3c311a0a65054cb67a1f4b3a04f9
Murlidhar Pathak <47478882+murlidharpathakprogrammer@users.noreply.github.com>
2021-11-01 22:38:42 +0530

    Update 1295-Find-Numbers-with-Even-Number-of-Digits.java

--- 1295-Find-Numbers-with-Even-Number-of-Digits.java
+++ 1295-Find-Numbers-with-Even-Number-of-Digits.java
@@ -3,3 +3,17 @@ Problem: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
          Given an array nums of integers, return how many of them contain an even number of digits.
 My Submission: https://leetcode.com/submissions/detail/580529526/
 */
+class Solution {
+    public int findNumbers(int[] nums) {
+        int res=0;
+        for(int each:nums){
+            int dig=0;
+            while(each>0){
+                dig++;
+                each/=10;
+            }
+            if(dig%2==0) res++;
+        }
+        return res;
+    }
+}
